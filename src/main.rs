4eea98b97f70c4b2da6597eb89f01373ef7e5382
Yann Dirson <ydirson@free.fr>
2023-12-16 15:14:24 +0100

    [TUTO] Use Rust language constructs in the markup

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,52 @@
 use yew::prelude::*;
 
+struct Video {
+    id: usize,
+    title: String,
+    speaker: String,
+    url: String,
+}
+
 #[function_component(App)]
 fn app() -> Html {
+
+    let videos = vec![
+        Video {
+            id: 1,
+            title: "Building and breaking things".to_string(),
+            speaker: "John Doe".to_string(),
+            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+        },
+        Video {
+            id: 2,
+            title: "The development process".to_string(),
+            speaker: "Jane Smith".to_string(),
+            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+        },
+        Video {
+            id: 3,
+            title: "The Web 7.0".to_string(),
+            speaker: "Matt Miller".to_string(),
+            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+        },
+        Video {
+            id: 4,
+            title: "Mouseless development".to_string(),
+            speaker: "Tom Jerry".to_string(),
+            url: "https://youtu.be/PsaFVLr8t4E".to_string(),
+        },
+    ];
+
+    let videos = videos.iter().map(|video| html! {
+        <p key={video.id}>{format!("{}: {}", video.speaker, video.title)}</p>
+    }).collect::<Html>();
+
     html! {
         <>
             <h1>{ "RustConf Explorer" }</h1>
             <div>
                 <h3>{"Videos to watch"}</h3>
-                <p>{ "John Doe: Building and breaking things" }</p>
-                <p>{ "Jane Smith: The development process" }</p>
-                <p>{ "Matt Miller: The Web 7.0" }</p>
-                <p>{ "Tom Jerry: Mouseless development" }</p>
+                { videos }
             </div>
             <div>
                 <h3>{ "John Doe: Building and breaking things" }</h3>
